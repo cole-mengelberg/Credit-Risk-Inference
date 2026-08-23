4044ddcf5074d58e680386a62622646a29e285c8
Cole.M <cole.mengelberg1@gmail.com>
2026-08-24 03:54:22 +0900

    Create linear regresion model to impute missing values in analysis.R

--- R/analysis.R
+++ R/analysis.R
@@ -1,19 +1,22 @@
-library(tidyverse)
+# =====================================================
+# 1. Load Data & Packages
+# =====================================================
 
+library(tidyverse)
 data <- read.csv("data/credit_risk_dataset.csv")
-               
+
+# =====================================================
+# 2. Data Cleaning
+# =====================================================
+
 view(data)
-#-----------------------------------------------------
-# Data Cleaning
-#-----------------------------------------------------
 
 #Check Na's
 colSums(is.na(data))
-sum(is.na(data)) / count(data) * 100
-
-#-----------------------------------------------------
-# Try to find pattern to fill Na's
-#-----------------------------------------------------
+sum(duplicated(data)) # 165 duplicates
+data <- unique(data)
+sum(!complete.cases(data)) / nrow(data) * 100 # 12% of rows have at least one missing value
+sum(is.na(data)) / (nrow(data) * ncol(data)) * 100 # 1% of the data is empty
 
 # Create feature to distinguish NA types
 temp_data <-
@@ -27,6 +30,7 @@ temp_data <-
       )
   )
 
+# Distribution of NA combinations
 temp_data|> 
   group_by(type) |> 
   summarise(
@@ -39,21 +43,6 @@ get_mode <- function(x) {
   names(sort(table(x), decreasing = TRUE))[1]
 }
 
-options(tibble.width = Inf)
-temp_data |> 
-  group_by(type) |> 
-  summarise(
-    median_age = median(person_age),
-    median_income = median(person_income),
-    mode_home_ownership = get_mode(person_home_ownership),
-    mode_loan_intent = get_mode(loan_intent),
-    mode_loan_grade = get_mode(loan_grade),
-    median_loan_amount = median(loan_amnt),
-    median_loan_percent_income = median(loan_percent_income),
-    mode_default_on_file = get_mode(cb_person_default_on_file),
-    median_hist_length = median(cb_person_cred_hist_length)
-  ) |> 
-  view()
 # Plot NA vs numerical features
 
 temp_data |> 
@@ -63,6 +52,12 @@ temp_data |>
 temp_data |> 
   ggplot(aes(x = person_income, color = type, fill = type, alpha = 0.2)) +
   geom_density()
+
+temp_data |> 
+  filter(type != "Both NA") |> 
+  ggplot(aes(x = person_income, color = type, fill = type, alpha = 0.2)) +
+  geom_density()
+
 
 temp_data |> 
   ggplot(aes(x = loan_amnt, color = type, fill = type, alpha = 0.2)) +
@@ -98,3 +93,138 @@ temp_data |>
   ggplot(aes(x = type, color = factor(cb_person_default_on_file), fill = factor(cb_person_default_on_file))) +
   geom_bar(position = "fill")
 
+# Statistics of NA feature groups
+options(tibble.width = Inf)
+temp_data |> 
+  group_by(type) |> 
+  summarise(
+    median_age = median(person_age),
+    median_income = median(person_income),
+    mode_home_ownership = get_mode(person_home_ownership),
+    mode_loan_intent = get_mode(loan_intent),
+    mode_loan_grade = get_mode(loan_grade),
+    median_loan_amount = median(loan_amnt),
+    median_loan_percent_income = median(loan_percent_income),
+    mode_default_on_file = get_mode(cb_person_default_on_file),
+    median_hist_length = median(cb_person_cred_hist_length)
+  ) |> 
+  view()
+
+temp_data |> 
+  group_by(type) |> 
+  count(person_home_ownership) |> 
+  mutate(proportion = n / sum(n) * 100) |> 
+  arrange(type, desc(proportion)) |> 
+  view()
+
+temp_data |> 
+  group_by(type) |> 
+  count(loan_intent) |> 
+  mutate(proportion = n / sum(n) * 100) |> 
+  arrange(type, desc(proportion)) |> 
+  view()
+
+temp_data |> 
+  group_by(type) |> 
+  count(loan_grade) |> 
+  mutate(proportion = n / sum(n) * 100) |> 
+  arrange(type, desc(proportion)) |> 
+  view()
+
+temp_data |> 
+  group_by(person_home_ownership) |> 
+  summarize(
+    median_int_rate = median(loan_int_rate, na.rm = TRUE),
+    median_emp_length = median(person_emp_length, na.rm = TRUE)
+  )
+
+temp_data |> 
+  group_by(loan_grade) |> 
+  summarize(
+    median_int_rate = median(loan_int_rate, na.rm = TRUE),
+    median_emp_length = median(person_emp_length, na.rm = TRUE)
+  )
+
+temp_data |> 
+  group_by(loan_intent) |> 
+  summarize(
+    median_int_rate = median(loan_int_rate, na.rm = TRUE),
+    median_emp_length = median(person_emp_length, na.rm = TRUE),
+  )
+
+# Use linear regression to impute the missing data
+
+lm_data <- temp_data |> 
+  filter(type != "Both NA")
+
+# Model for loan interest rate
+int_rate_model <- lm(
+  loan_int_rate ~
+    person_age +
+    person_income +
+    person_home_ownership +
+    loan_intent +
+    loan_grade +
+    loan_amnt +
+    loan_percent_income +
+    cb_person_cred_hist_length +
+    cb_person_default_on_file +
+    person_emp_length,
+  data = lm_data
+)
+
+# Model for employment length
+emp_model <- lm(
+  person_emp_length ~
+    person_age +
+    person_income +
+    person_home_ownership +
+    loan_intent +
+    loan_grade +
+    loan_amnt +
+    loan_percent_income +
+    cb_person_cred_hist_length +
+    cb_person_default_on_file +
+    loan_int_rate,
+  data = lm_data
+)
+
+# Fill missing values
+
+data <- data |>
+  mutate(
+    loan_int_rate = if_else(
+      is.na(loan_int_rate) & !is.na(person_emp_length),
+      predict(
+        int_rate_model,
+        newdata = data
+      ),
+      loan_int_rate
+    )
+  )
+
+data <- data |>
+  mutate(
+    person_emp_length = if_else(
+      is.na(person_emp_length) & !is.na(loan_int_rate),
+      predict(
+        emp_model,
+        newdata = data
+      ),
+      person_emp_length
+    )
+  )
+
+data <- data |>
+  mutate(
+    loan_int_rate = if_else(
+      is.na(loan_int_rate),
+      median(loan_int_rate, na.rm = TRUE),
+      loan_int_rate
+    ),
+    person_emp_length = if_else(
+      is.na(person_emp_length),
+      median(person_emp_length, na.rm = TRUE),
+      person_emp_length
+    )
+  )
